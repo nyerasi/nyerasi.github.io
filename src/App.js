422fa634757348b526885449e8017a89449a9ac9
Nik Yerasi <nyerasi@yahoo.com>
2019-11-07 21:39:48 -0800

    completed overhaul of design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import Header from './components/Header.js'
-import NavigationBar from './components/NavigationBar.js'
-import SplashPage from './components/SplashPage.js'
+import SplashPage from './components/SplashPage.js';
 
-import logo from './logo.svg';
-import './App.css';
+import styled, { css } from 'styled-components';
+
+const Body = styled.div`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+`;
 
 function App() {
   return (
-      <div className='app'>
-        <NavigationBar/>
-        <Header/>
-        <div className='projects' style={{ height:1000, backgroundImage:'linear-gradient(#212121, #848484)' }} />
-      </div>
+    <Body>
+      <SplashPage/>
+    </Body>
   );
 }
 
