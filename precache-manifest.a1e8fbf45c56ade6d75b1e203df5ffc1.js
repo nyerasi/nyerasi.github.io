8aeef404a97732776c11afcb0e24761fda7b1f7f
Nik Yerasi <nyerasi@yahoo.com>
2019-11-07 22:12:09 -0800

    Updates

---
+++ precache-manifest.a1e8fbf45c56ade6d75b1e203df5ffc1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ee4022a120337bf6062a71df3aaca4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c4a098947ab92250968",
+    "url": "/static/js/2.8fc6a2d6.chunk.js"
+  },
+  {
+    "revision": "404a22aa8b4d1dd8c2c5",
+    "url": "/static/js/main.8b9b8a0b.chunk.js"
+  },
+  {
+    "revision": "e676c0768ba9136fc741",
+    "url": "/static/js/runtime~main.daaf4d4d.js"
+  }
+]);
