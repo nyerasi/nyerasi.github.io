25ae447b77dce059ee9f875e0d0444b3130820aa
Nik Yerasi <nyerasi@yahoo.com>
2020-05-27 09:04:17 -0700

    Updates

---
+++ precache-manifest.a8983e448c849ff26ebbdec69e02bcb9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f8e1c41a5ed44da5f47c791c844242",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9a6112f98aac1caaa1f",
+    "url": "/static/js/2.11d2a30f.chunk.js"
+  },
+  {
+    "revision": "980d97084a235ef2cc0e",
+    "url": "/static/js/main.70b80b8f.chunk.js"
+  },
+  {
+    "revision": "eaa5680ce842acc691fe",
+    "url": "/static/js/runtime~main.dd8497c4.js"
+  },
+  {
+    "revision": "4e1657d878440d9d9e0f6fd3c451b321",
+    "url": "/static/media/bampfa_mockup.4e1657d8.png"
+  },
+  {
+    "revision": "bb32d0d51e4d2ee85f003bac418ce9e0",
+    "url": "/static/media/genomics_mockup.bb32d0d5.png"
+  },
+  {
+    "revision": "1b2d2c4e0d65e494f370ce2ab075ba05",
+    "url": "/static/media/neomotion_mockup.1b2d2c4e.png"
+  },
+  {
+    "revision": "1c1d6973801a642bea6b784f7606cb65",
+    "url": "/static/media/scatter_mockup.1c1d6973.png"
+  },
+  {
+    "revision": "0a6030752e6e655a63f63161fa37c983",
+    "url": "/static/media/snapagram_mockup.0a603075.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7eea1011d44e12783d338484f481e53f.js"
+  "/precache-manifest.a8983e448c849ff26ebbdec69e02bcb9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
